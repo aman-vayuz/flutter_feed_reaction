c59a90597015c4be781aabebeec1e6ec59df6b09
aman-punj <apsingh.amanpreet@gmail.com>
2024-08-29 21:47:32 +0530

    reaction name , id in call back of FeedReaction

--- lib/models/feed_reaction_model.dart
+++ lib/models/feed_reaction_model.dart
@@ -4,10 +4,14 @@ class FeedReaction {
   final int id;
   final Widget header;
   final Widget reaction;
+  final String reactionName;
+  final String reactionId;
 
   FeedReaction({
     required this.id,
     required this.header,
     required this.reaction,
+    required this.reactionId,
+    required this.reactionName
   });
 }
